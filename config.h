cc345b07811b88086328d16e398726a6df6990fe
Travis Cole <11240+kelp@users.noreply.github.com>
2019-09-22 19:43:23 -0700

    Add icons for some work spaces.
    Remove extra Rules that I don't need.
    
    Remove a commented out termcmd line.

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "3", "4", "5", "6", "7", "", "" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -29,11 +29,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Chromium", NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Xonix",    NULL,       NULL,       0,            1,           -1 },
-	{ NULL,       NULL,       "glxgears", 0,            1,           -1 },
+	{ "Chromium", NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,7 +59,6 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-//static const char *termcmd[]  = { "st -f 'Hack Regular Nerd Font Complete Mono'", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
